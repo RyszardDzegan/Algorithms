69e7874797bcb648ea952b4bbcf193776cc7974b
Ryszard Dżegan <ryszarddzegan@wp.pl>
2015-09-21 08:01:14 +0200

    Add matrix rotator algorithm

---
+++ Algorithms/Matrices.Tests/RotatorTests.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using NUnit.Framework;
+using System.Linq;
+
+namespace Algorithms.Matrices.Tests
+{
+    [TestFixture]
+    public class RotatorTests
+    {
+        [Test]
+        public void Test0()
+        {
+            var matrix = new int[0, 0];
+            var expected = new int[0, 0];
+            var sut = new Rotator<int>(matrix);
+            sut.Rotate();
+            AssertMatrixIsRotated(expected, matrix);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var matrix = new[,] { { 1 } };
+            var expected = new[,] { { 1 } };
+            var sut = new Rotator<int>(matrix);
+            sut.Rotate();
+            AssertMatrixIsRotated(expected, matrix);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var matrix = new[,]
+                {{ 1, 2 },
+                 { 4, 3 }};
+
+            var expected = new[,]
+                {{ 4, 1 },
+                 { 3, 2 }};
+
+            var sut = new Rotator<int>(matrix);
+            sut.Rotate();
+            AssertMatrixIsRotated(expected, matrix);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var matrix = new[,]
+                {{ 1, 2, 3 },
+                 { 8, 9, 4 },
+                 { 7, 6, 5 }};
+
+            var expected = new[,]
+                {{ 8, 1, 2 },
+                 { 7, 9, 3 },
+                 { 6, 5, 4 }};
+
+            var sut = new Rotator<int>(matrix);
+            sut.Rotate();
+            AssertMatrixIsRotated(expected, matrix);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            var matrix = new[,]
+                {{  1,  2,  3,  4 },
+                 { 12, 13, 14,  5 },
+                 { 11, 16, 15,  6 },
+                 { 10,  9,  8,  7 }};
+
+            var expected = new[,]
+                {{ 12,  1,  2,  3 },
+                 { 11, 16, 13,  4 },
+                 { 10, 15, 14,  5 },
+                 {  9,  8,  7,  6 }};
+
+            var sut = new Rotator<int>(matrix);
+            sut.Rotate();
+            AssertMatrixIsRotated(expected, matrix);
+        }
+
+        [Test]
+        public void Test5()
+        {
+            var matrix = new[,]
+                {{ 1, 2 },
+                 { 4, 3 }};
+
+            var expected = new[,]
+                {{ 3, 4 },
+                 { 2, 1 }};
+
+            var sut = new Rotator<int>(matrix);
+            sut.Rotate();
+            sut.Rotate();
+            AssertMatrixIsRotated(expected, matrix);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException), ExpectedMessage = "Parameter \"matrix\" must have the same number of columns and rows, but it has 3 rows and 2 columns.")]
+        public void Test6()
+        {
+            var matrix = new[,]
+                {{ 1, 2 },
+                 { 3, 4 },
+                 { 5, 6 }};
+
+            var sut = new Rotator<int>(matrix);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException), UserMessage = "Parameter \"matrix\" must have the same number of columns and rows, but it has 2 rows and 3 columns.")]
+        public void Test7()
+        {
+            var matrix = new[,]
+                {{ 1, 2, 3 },
+                 { 4, 5, 6 }};
+
+            var sut = new Rotator<int>(matrix);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException), UserMessage = "Parameter \"matrix\" can't be null.")]
+        public void Test8()
+        {
+            var sut = new Rotator<int>(null);
+        }
+
+        private static void AssertMatrixIsRotated<T>(T[,] expected, T[,] actual)
+        {
+            Assert.AreEqual(expected.GetLength(0), actual.GetLength(0));
+            Assert.AreEqual(expected.GetLength(1), actual.GetLength(1));
+
+            var expectedString = MatrixToString(expected);
+            var actualString = MatrixToString(actual);
+
+            Assert.AreEqual(expectedString, actualString);
+        }
+
+        private static string MatrixToString<T>(T[,] matrix) =>
+            string.Join(Environment.NewLine, Enumerable.Range(0, matrix.GetLength(0)).Select(i => string.Join(" ", Enumerable.Range(0, matrix.GetLength(1)).Select(j => matrix[i, j]))));
+    }
+}
---
+++ Algorithms/Matrices/Rotator.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Algorithms.Matrices
+{
+    /// <summary>
+    /// Performs a clockwise matrix rotation.
+    /// </summary>
+    class Rotator<T>
+    {
+        /// <summary>
+        /// A mutable matrix provided in constructor.
+        /// Its value is updated after each <see cref="Rotate"/> invocation.
+        /// </summary>
+        public T[,] Matrix { get; }
+
+        /// <summary>
+        /// A constructor.
+        /// </summary>
+        /// <param name="matrix">
+        /// A matrix to rotate.
+        /// Its row count must equal its column count.
+        /// </param>
+        public Rotator(T[,] matrix)
+        {
+            if (matrix == null)
+                throw new ArgumentNullException($"Parameter \"{nameof(matrix)}\" can't be null.");
+
+            if (matrix.GetLength(0) != matrix.GetLength(1))
+                throw new ArgumentException($"Parameter \"{nameof(matrix)}\" must have the same number of columns and rows, but it has {matrix.GetLength(0)} rows and {matrix.GetLength(1)} columns.");
+
+            Matrix = matrix;
+        }
+
+        /// <summary>
+        /// Rotates the <see cref="Matrix"/> in a clockwise manner.
+        /// It uses "divide and conquer" algorithm.
+        /// The matrix is divided into unrelated rings like an onion.
+        /// Then it rotates all rings in parallel.
+        /// </summary>
+        public void Rotate()
+        {
+            var rotations =
+                from level in Enumerable.Range(0, Matrix.GetLength(0) / 2)
+                select new Action(() => RotateRing(level));
+
+            Parallel.ForEach(rotations, rotation => rotation());
+        }
+
+        /// <summary>
+        /// An asynchronuos version of <see cref="Rotate"/>.
+        /// </summary>
+        /// <returns></returns>
+        public Task RotateAsync() =>
+            Task.Run(() => Rotate());
+
+        private void RotateRing(int level)
+        {
+            var begin = level;
+            var count = Matrix.GetLength(0) - level;
+            var end = count - 1;
+
+            var temp = Matrix[begin, begin];
+
+            for (var i = 0; i < count - 1; i++)
+                Matrix[begin + i, begin] = Matrix[begin + i + 1, begin];
+
+            for (var i = 0; i < count - 1; i++)
+                Matrix[end, begin + i] = Matrix[end, begin + i + 1];
+
+            for (var i = 0; i < count - 1; i++)
+                Matrix[end - i, end] = Matrix[end - i - 1, end];
+
+            for (var i = 0; i < count - 2; i++)
+                Matrix[begin, end - i] = Matrix[begin, end - i - 1];
+
+            Matrix[begin, begin + 1] = temp;
+        }
+    }
+}
